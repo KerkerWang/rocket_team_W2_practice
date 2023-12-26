5b01a7fc6b8deb3da8820c61072559600cc94de3
Ya-Hsien Wang <109192255+KerkerWang@users.noreply.github.com>
2023-12-26 10:55:50 +0800

    page5,7,9,11 upload

--- W2_practice/Program.cs
+++ W2_practice/Program.cs
@@ -4,7 +4,96 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        //page5
+        //讀入兩個數x和y，選出大的那一個
+        //double x, y;
+        //Console.WriteLine("請輸入數字x");
+        //x = Convert.ToDouble(Console.ReadLine());
+        //Console.WriteLine("請輸入數字y");
+        //y = Convert.ToDouble(Console.ReadLine());
+        //if (x == y)
+        //{
+        //    Console.WriteLine("兩數相等");
+        //}
+        //else if (x > y)
+        //{
+        //    Console.WriteLine($"數字x:{x}大於數字y:{y}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"數字x:{x}小於數字y:{y}");
+        //}
+        //Console.ReadKey();
+
+
+        //page7
+        //讀入兩個數x和y，選出大的那一個
+        //double x, y;
+        //Console.WriteLine("請輸入數字x");
+        //x = Convert.ToDouble(Console.ReadLine());
+        //Console.WriteLine("請輸入數字y");
+        //y = Convert.ToDouble(Console.ReadLine());
+        //if (x == y)
+        //{
+        //    Console.WriteLine("兩數相等");
+        //}
+        //else if (x > y)
+        //{
+        //    Console.WriteLine($"數字x:{x}大於數字y:{y}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"數字x:{x}小於數字y:{y}");
+        //}
+        //Console.ReadKey();
+
+
+        //page9
+        //讀入兩個數x和y，選出大的那一個
+        //double x, y;
+        //Console.WriteLine("請輸入數字x");
+        //x = Convert.ToDouble(Console.ReadLine());
+        //Console.WriteLine("請輸入數字y");
+        //y = Convert.ToDouble(Console.ReadLine());
+        //if (x == y)
+        //{
+        //    Console.WriteLine("兩數相等");
+        //}
+        //else if (x > y)
+        //{
+        //    Console.WriteLine($"數字x:{x}大於數字y:{y}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"數字x:{x}小於數字y:{y}");
+        //}
+        //Console.ReadKey();
+
+
+        //page11
+        //讀入兩個數x和y，選出大的那一個
+        //double x, y;
+        //Console.WriteLine("請輸入數字x");
+        //x = Convert.ToDouble(Console.ReadLine());
+        //Console.WriteLine("請輸入數字y");
+        //y = Convert.ToDouble(Console.ReadLine());
+        //if (x == y)
+        //{
+        //    Console.WriteLine("兩數相等");
+        //}
+        //else if (x > y)
+        //{
+        //    Console.WriteLine($"數字x:{x}大於數字y:{y}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"數字x:{x}小於數字y:{y}");
+        //}
+        //Console.ReadKey();
+
+
+
     }
 }
 
